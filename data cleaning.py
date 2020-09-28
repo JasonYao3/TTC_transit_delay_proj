6eb0eb9e470fe2634adf22d3aef059153daeff94
Jason Yao <jasonyao105025@gmail.com>
2020-09-28 12:35:37 -0400

    uploaded files

---
+++ data cleaning.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 12:20:11 2020
+
+@author: Jason
+"""
+
+import pandas as pd
+
+#bus_df = pd.read_csv('./data/merged_bus.csv', encoding='Latin-1')
+subway_df = pd.read_csv('./data/merged_subway.csv', encoding='Latin-1')
